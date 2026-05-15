3bc89f94f38d96fdd036a8eaadcd53802daed7df
abdunnoorfaheem <abdunnoor3030@gmail.com>
2026-05-15 19:50:22 +0600

    some problems added

--- Day 12/js/script.js
+++ Day 12/js/script.js
@@ -14,3 +14,36 @@
 // delete people["isLegend"];
 // console.log(people)
 
+// let player={
+//     name:"Messi",
+//     sports:"Football",
+//     worldCup:1,
+// }
+// let {name,sports,worldCup}=player;
+// console.log(worldCup)
+
+
+
+// let arr=["one","two","three"];
+// let[first,second,threee]=arr;
+
+// console.log(first,second,threee)
+
+
+let a=10;
+let b=20;
+let c;
+
+c=b;
+b=a;
+a=c
+
+console.log(a)
+console.log(b)
+// console.log(c)
+
+// [a,b]=[b,a];
+
+
+console.log("a:",a," b:",b)
+
