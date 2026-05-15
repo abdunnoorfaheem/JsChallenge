2cea741fa3fa8c865bbeba8d0ad5a1dd4bce6d5a
abdunnoorfaheem <abdunnoor3030@gmail.com>
2026-05-15 12:37:55 +0600

    topic object

---
+++ Day 12/js/script.js
@@ -0,0 +1,16 @@
+// let people={
+//     name:"Noor",
+//     age:20,
+// }
+// console.log(people)
+// console.log(people.name)
+// console.log(people["age"])
+// people.address="Dhaka";
+// console.log(people)
+// people.isLegend=true;
+// console.log(people);
+// delete people.address;
+// console.log(people)
+// delete people["isLegend"];
+// console.log(people)
+
